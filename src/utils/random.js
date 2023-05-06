0489a43bb5e3acdede420a80ff0429ef710b9233
Ricardo <LQS5858@163.COM>
2023-05-06 12:49:53 +0800

    Create random.js

---
+++ src/utils/random.js
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @param {*} options {prefix:接口前缀,n:随机字符长度}
+ * @returns 
+ */
+export function random (options = {}) {
+  const { prefix, n } = options || {}
+  if (!n) return
+  let str = '0123456789abcdefghijklmopqrstuvwxyz'
+  let result = ''
+  for (let i = 0; i < n; i++) {
+    result += str?.charAt(Math.round(Math.random() * str?.length))
+  }
+  return `${prefix}${result}`
+}
